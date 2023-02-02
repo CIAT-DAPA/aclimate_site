b9a9f6e8e97ec751576cfde1cab43494c3b4e177
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-02-02 13:27:18 -0500

    router changed

--- src/src/App.js
+++ src/src/App.js
@@ -1,5 +1,5 @@
 import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Menu from "./components/menu/Menu";
 import Partners from "./pages/partners/Partners";
@@ -7,10 +7,10 @@ import News from "./pages/news/News";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/aclimate_site">
       <Menu />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/Partners" element={<Partners />} />
         <Route path="/News" element={<News />} />
       </Routes>
