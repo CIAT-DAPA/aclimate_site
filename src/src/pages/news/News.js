acf291da17904366a8ac60695d99b622f3130ff0
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-02-02 11:15:17 -0500

    add gh-pages

--- src/src/pages/news/News.js
+++ src/src/pages/news/News.js
@@ -26,22 +26,28 @@ function News() {
 
   return (
     <div>
-    {news.map((e, index) => (
-      <Row style={{ height: "10rem" }}>
-      <Col className="h-100 col-2">
-        <img src={Logo} className="img-fluid h-100" />
-      </Col>
-      <Col>
-        <h5>{e.Titulo}</h5>
-        <p class="text-muted">Date: {e.Fecha}</p>
-        <p className="fs-6">
-          {e.Resumen}
-        </p>
-        <Button variant="primary" href={e.Link} target="_blank">Go to article</Button>
-      </Col>
-    </Row>
-    ))}
-    
+      <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
+        <div class="col-md-6 px-0">
+          <h1 class="display-4">
+            News about Aclimate
+          </h1>
+        </div>
+      </div>
+      {news.map((e, index) => (
+        <Row style={{ height: "10rem" }} key={index} className="m-0 px-4">
+          <Col className="h-100 col-2">
+            <img src={Logo} className="img-fluid h-100" />
+          </Col>
+          <Col className="p-0">
+            <h5>{e.Titulo}</h5>
+            <p class="text-muted">Date: {e.Fecha}</p>
+            <p className="fs-6">{e.Resumen}</p>
+            <Button variant="primary" href={e.Link} target="_blank">
+              Go to article
+            </Button>
+          </Col>
+        </Row>
+      ))}
     </div>
   );
 }
