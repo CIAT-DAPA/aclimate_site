deafd4831b1828fa6ce948f123059b88d12e6c11
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-02-02 11:56:35 -0500

    router changed

--- src/src/App.js
+++ src/src/App.js
@@ -1,18 +1,18 @@
-import Home from './pages/home/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Menu from './components/menu/Menu'
-import Partners from './pages/partners/Partners';
-import News from './pages/news/News';
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Menu from "./components/menu/Menu";
+import Partners from "./pages/partners/Partners";
+import News from "./pages/news/News";
 
 function App() {
   return (
     <Router>
       <Menu />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Partners' element={<Partners />} />
-        <Route path='/News' element={<News />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Partners" element={<Partners />} />
+        <Route path="/News" element={<News />} />
       </Routes>
     </Router>
   );
