b756b67031c53a258fe43955cd2997c1bc94b632
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2023-02-02 10:08:01 -0500

    News edited

--- src/src/pages/news/News.js
+++ src/src/pages/news/News.js
@@ -2,8 +2,28 @@ import "./News.css";
 import Button from "react-bootstrap/Button";
 import Logo from "../../assets/images/logo.png";
 import { Col, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+
+const URL_NEWS_DATA = "https://github.com/CIAT-DAPA/aclimate_site/blob/main/data/news.csv";
 
 function News() {
+
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    Papa.parse(URL_NEWS_DATA, {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: (results) => {
+        setNews(results.data);
+      },
+    });
+  }, []);
+
+  console.log(news);
+
   return (
     <Row style={{ height: "10rem" }}>
       <Col className="h-100 col-2">
