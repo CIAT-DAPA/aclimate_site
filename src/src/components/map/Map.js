1a7d16222645e9ac25bb658fb50996835d6a2618
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2023-03-31 11:10:40 -0500

    AC-130 map icons improved

--- src/src/components/map/Map.js
+++ src/src/components/map/Map.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
+import { Icon } from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./Map.css";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import markerImg from "../../assets/images/marker.png";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const URL_CSV_COUNTRIES =
   "https://raw.githubusercontent.com/CIAT-DAPA/aclimate_site/main/data/countries.csv";
 
+const customIcon = new Icon({
+  iconUrl: markerImg,
+  iconSize: [48, 60],
+  iconAnchor: [24, 60],
+});
+
 function Map() {
-  const [t, i18n] = useTranslation("global")
+  const [t, i18n] = useTranslation("global");
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
@@ -30,19 +41,32 @@ function Map() {
       style={{ height: "100%" }}
       className="hidden"
       zoomControl={false}
-      maxBounds={[[79.36, -157.89], [-38.67, 165.44]]}
+      maxBounds={[
+        [79.36, -157.89],
+        [-38.67, 165.44],
+      ]}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {countries.map((country, index) => (
-        <Marker key={index} position={[country.Latitud, country.Longitud]}>
-          <Popup>
-            {country.Pais} <br />
-            <a className="btn btn-primary btn-sm text-light" href={country.Link} target="_blank" rel="noreferrer" role="button">{t("map.to-site")}</a>
+        <Marker
+          key={index}
+          position={[country.Latitud, country.Longitud]}
+          icon={customIcon}
+        >
+          <Popup className="">
+            <a
+              className="text-dark text-decoration-none fs-6"
+              href={country.Link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {country.Pais}
+              <Button variant="primary" className="btn-sm ms-2 rounded-circle"><FontAwesomeIcon icon={faArrowRight} className="icon-popup" /></Button>
+            </a>
           </Popup>
         </Marker>
-      ))
-      }
-    </MapContainer >
+      ))}
+    </MapContainer>
   );
 }
 
