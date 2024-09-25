8854b3205f143354923ce9ae742d34c71bf4292d
Sebastian Lopez <sebastian.lopez_ast@uao.edu.co>
2024-09-25 11:20:22 -0500

    AC-249 new added and sort date fixed

--- src/src/pages/articles/Articles.js
+++ src/src/pages/articles/Articles.js
@@ -1,50 +1,60 @@
 import "./Articles.css";
 import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
 import New from "../../components/new/New";
 import Footer from "../../components/footer/Footer";
 
 const URL_NEWS_DATA =
-    "https://raw.githubusercontent.com/CIAT-DAPA/aclimate_site/main/data/articles.csv";
-
+  "https://raw.githubusercontent.com/CIAT-DAPA/aclimate_site/main/data/articles.csv";
 
 function Articles() {
-    const [t, i18n] = useTranslation("global")
-    const [articles, setArticles] = useState([]);
+  const [t, i18n] = useTranslation("global");
+  const [articles, setArticles] = useState([]);
 
-    useEffect(() => {
-        Papa.parse(URL_NEWS_DATA, {
-            download: true,
-            header: true,
-            dynamicTyping: true,
-            complete: (results) => {
-                setArticles(results.data);
-            },
-        });
-    }, []);
+  useEffect(() => {
+    Papa.parse(URL_NEWS_DATA, {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: (results) => {
+        setArticles(results.data);
+      },
+    });
+  }, []);
 
-    return (
-        <div>
-            <div className="mb-4 text-white bg-title">
-                <div className="container pb-3 px-4 container-news" style={{}}>
-                    <div className="col-md-6 px-0">
-                        <h1 className="display-4">
-                            {t("articles.articles-title")}
-                        </h1>
-                    </div>
-                </div>
-            </div>
-            {
-                articles
-                    .sort((a, b) => new Date(b.Fecha) - new Date(a.Fecha))
-                    .map((e, index) => (
-                        <New key={index} title={e.Titulo} date={e.Fecha} summaryEs={e.ResumenEs} summaryEn={e.ResumenEn} link={e.Link} delay={index * 100} />
-                    ))
-            }
-            <Footer></Footer>
-        </div >
-    )
+  return (
+    <div>
+      <div className="mb-4 text-white bg-title">
+        <div className="container pb-3 px-4 container-news" style={{}}>
+          <div className="col-md-6 px-0">
+            <h1 className="display-4">{t("articles.articles-title")}</h1>
+          </div>
+        </div>
+      </div>
+      {articles
+        .sort((a, b) => {
+          const [dayA, monthA, yearA] = a.Fecha.split("/").map(Number);
+          const [dayB, monthB, yearB] = b.Fecha.split("/").map(Number);
+          return (
+            new Date(yearB, monthB - 1, dayB) -
+            new Date(yearA, monthA - 1, dayA)
+          );
+        })
+        .map((e, index) => (
+          <New
+            key={index}
+            title={e.Titulo}
+            date={e.Fecha}
+            summaryEs={e.ResumenEs}
+            summaryEn={e.ResumenEn}
+            link={e.Link}
+            delay={index * 100}
+          />
+        ))}
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default Articles;
--- src/src/pages/news/News.js
+++ src/src/pages/news/News.js
@@ -2,7 +2,7 @@ import "./News.css";
 import React, { useState, useEffect } from "react";
 import Papa from "papaparse";
 import New from "../../components/new/New";
-import { useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next";
 import Footer from "../../components/footer/Footer";
 
 const URL_NEWS_DATA =
@@ -30,21 +30,33 @@ function News() {
       <div className="mb-4 text-white bg-title">
         <div className="container pb-3 px-4 container-news" style={{}}>
           <div className="col-md-6 px-0">
-            <h1 className="display-4">
-              {t("news.news-title")}
-            </h1>
+            <h1 className="display-4">{t("news.news-title")}</h1>
           </div>
         </div>
       </div>
-      {
-        news
-          .sort((a, b) => new Date(b.Fecha) - new Date(a.Fecha))
-          .map((e, index) => (
-            <New key={index} image={e.Image} title={e.Titulo} date={e.Fecha} summaryEs={e.ResumenEs} summaryEn={e.ResumenEn} link={e.Link} delay={index * 100} />
-          ))
-      }
+      {news
+        .sort((a, b) => {
+          const [dayA, monthA, yearA] = a.Fecha.split("/").map(Number);
+          const [dayB, monthB, yearB] = b.Fecha.split("/").map(Number);
+          return (
+            new Date(yearB, monthB - 1, dayB) -
+            new Date(yearA, monthA - 1, dayA)
+          );
+        })
+        .map((e, index) => (
+          <New
+            key={index}
+            image={e.Image}
+            title={e.Titulo}
+            date={e.Fecha}
+            summaryEs={e.ResumenEs}
+            summaryEn={e.ResumenEn}
+            link={e.Link}
+            delay={index * 100}
+          />
+        ))}
       <Footer></Footer>
-    </div >
+    </div>
   );
 }
 
